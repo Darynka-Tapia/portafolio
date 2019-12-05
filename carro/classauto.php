beced7ea3180e9d6964b5bf301c7b8f2e1117ec4
Darynka Tapia <darysosa09@gmail.com>
2019-12-05 12:55:09 -0600

    mas

---
+++ carro/classauto.php
@@ -0,0 +1,119 @@
+<?php 
+
+	interface Auto{
+		public function encender();
+		public function apagar();
+
+	}
+
+	interface gasolina extends Auto{
+		public function vaciarTanque();
+		public function llenarTanque($cant);
+	}
+
+	class Deportivo implements gasolina{
+		//Atributos
+		private $estado=false;
+		private $tanque=0;
+
+		//Métodos
+		public function estado(){
+			if($this->estado){
+				echo 'El auto esta encendido y tiene '.$this->tanque.' de litros en el tanque<br>';
+			}else{
+				echo 'El auto esta apagado <br>';
+			}
+			if($this->tanque<0){
+				$this->tanque=0;
+			}
+			echo '<br>Gasolina: '.$this->tanque.'<br>';
+		}
+		public function encender(){
+			if($this->estado){
+				echo "No pueden encender el auto dos veces <br>";
+			}else{
+				if($this->tanque>0){
+					echo 'Auto encendido <br>';
+					$this->estado=true;
+				}else{
+					echo 'El tanque esta vacio, no puedes encender el tanque <br>';
+				}
+				
+			}
+
+		}
+		public function apagar(){
+			if($this->estado){
+				echo "Auto apagado <br>";
+				$this->estado=false;
+			}else{
+				echo '<br>El auto ya esta apagado ';
+				
+			}
+		}
+		public function vaciarTanque(){
+			$this->tanque=0;
+		}
+
+		public function llenarTanque($cant){
+			$this->tanque=$cant;
+		}
+
+		public function usar($km){
+			if($this->estado){
+				$reducir = $km/4;
+				$this->tanque = $this->tanque - $reducir;
+				if($this->tanque<=0){
+					$this->estado=false;
+				}
+			}else{
+				echo 'el auto esta apagado y no se puede usar <br>';
+			}
+		}
+
+
+	}
+
+	$obj = new Deportivo();	
+
+
+
+// 	//Comprobamos que el valor no venga vacío
+if(isset($_POST['funcion'])) {
+
+    $funcion = $_POST['funcion'];
+    $km=$_POST['km'];
+    $lt=$_POST['lt'];
+    if($lt<0){
+    	$lt=0;
+    }
+   
+
+    //En función del parámetro que nos llegue ejecutamos una función u otra
+    switch($funcion) {
+        case 'estado':
+            echo $obj->llenarTanque($lt);
+            echo $obj->estado();
+            break;
+        case 'encender': 
+        	echo $obj->llenarTanque($lt);
+            echo $obj->encender();
+            echo $obj->estado();
+            break;
+        case 'apagar': 
+            echo $obj->apagar();
+            echo $obj->estado();
+            break;
+        case 'llenar': 
+            echo $obj->llenarTanque($lt);
+            echo $obj->estado();
+            break;
+        case 'usar':
+        	echo $obj->llenarTanque($lt);
+        	echo $obj->encender();
+           	echo $obj->usar($km);
+           	echo $obj->estado();
+            break;
+    }
+}
+?>
---
+++ carro/ejercicio6.php
@@ -0,0 +1,84 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Carro</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+</head>
+<body background="ejercicio6IMG/Camino2.png">
+	<div class="container">
+
+		<div class="row" style="">
+			<div class="col-8 mt-3 pt-3 cajas">
+				<div class="row">
+					<div class="col-6">
+						<button type="button" class="btn btn-primary" onclick="obtener('encender')">Encender</button>
+						<button type="button" class="btn btn-danger" onclick="obtener('apagar')">Apagar</button>
+						<div class="form-group">
+							<label for="formControlRange">Km a recorrer</label>
+							<input type="range" class="form-control-range" id="formControlRange" value="0" onclick="obtener('usar')">
+						</div>
+					</div>
+					<div class="col-6">
+						<label for="">Agregar Gasolina (Lts.)</label>
+						<input type="number" min="0" max="100" value="" id="litros" onKeyUp="obtener('llenar')">
+						<button class="btn btn-info" onclick="obtener('estado')">Ver estado del auto</button>
+					</div>
+				</div>
+				
+				
+			</div>
+			<div class="col-4 mt-3 cajas" style=" width:auto; height:200px; overflow: scroll;">
+				<b>Mensaje	<br></b>
+				<div class="resultado" id="resultado"> </div> 
+				<div class="funciones" id="funciones">  <br></div> 
+			</div>
+
+		</div>
+	</div>
+
+
+	<script>
+
+
+		function obtener(funcion){
+			var kilometros=document.getElementById("formControlRange").value;
+			var litros=document.getElementById("litros").value;
+			
+
+
+			var parametros = {
+				"funcion" : funcion,
+				"km" :kilometros, 
+				"lt": litros
+
+			};
+
+			$.ajax({
+	            data:  parametros, //datos que se envian a traves de ajax
+	            url:   'classauto.php', //archivo que recibe la peticion
+	            type:  'post', //método de envio
+	            beforeSend: function () {
+	            	$("#resultado").html("Procesando, espere por favor...");
+	            },
+	            success:  function (response) { //una vez que el archivo recibe el request lo procesa y lo devuelve
+	            	$("#resultado").html(response);
+	            }
+	        });
+		}
+		
+
+
+	</script>
+	
+	<style>
+		.cajas{
+			border: 2px solid #000;
+		}
+	</style>
+</body>
+</html>
