5336cd1b1adf45ee7324c443b30808d4aa3235d3
Darynka Tapia <darysosa09@gmail.com>
2019-12-05 12:48:56 -0600

    subo el index

---
+++ index.php
@@ -0,0 +1,345 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="Nombre" content="Abril Darynka Tapia Sosa"/>
+    <meta name="Pseudo" content="Darynka Tapia"/>
+    <meta name="Objetivo" content="Desarrollo Web"/>
+    <meta name="Forma" content="Frelancer"/>
+
+    <meta name="Objetivo" content="Darynka-Tapia"/>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+      <link rel="stylesheet" href="estilo.css">
+      <link href="https://fonts.googleapis.com/css?family=Oswald:400,500,600,700&display=swap" rel="stylesheet"> 
+
+    <title>Darynka Tapia</title>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153543156-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-153543156-1');
+    </script>
+
+  </head>
+  <body>
+
+    <div class="container-fluid styleNav">
+      <nav class="navbar navbar-expand-lg ">
+        <a class="navbar-brand" href="index.php">Darynka</a>
+        <button class="navbar-toggler styleNav-icon" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+          <span class="navbar-toggler-icon ">&#9776;</span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+          <ul class="navbar-nav mr-auto" >
+            <li class="nav-item active" >
+              <a class="nav-link" href="#sobremi">Sobre mi <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#proyectos">Portafolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#certificados">Certificados</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    
+
+
+    <div class="container-fluid textos-contenedor">
+        <div class="textos">
+        <img src="perfil.jpg" alt="" class="perfil"> 
+
+        <h1>Darynka Tapia</h1>
+        <h2>Desarrollo web</h2>
+      </div>
+    </div>
+
+
+
+    <div class="container mt-5 pl-5 pr-5" id="sobremi">
+      <div class="row">
+        <div class="col-lg-8 col-md-12">
+          <br>
+          <br>
+          <br>
+          <h2>Acerca de mi</h2>
+          <p> Monterrey, México - darytapia09@gmail.com </p>
+
+          <p> Soy Abril Darynka Tapia Sosa, estudié la carrera de Tecnologías de la Información y me dedico al desarrollo web. <br>
+              Me considero una persona autodidacta que ama aprender y disfruta el realizar proyectos desafiantes. </p>
+
+          <p>
+            En mi tiempo libre mientras estaba en la Universidad lo aprovechaba para aprender más, entre semana me quedaba a clases extracurriculares de francés y los sábados asistía a un club de robótica en los cuales aprendí a manipular los motores y sensores del robot Nao además de las bases de electrónica.
+          </p>
+          <p> 
+            <span class="badge badge-info">HTML5</span> 
+            <span class="badge badge-info">CSS3</span> 
+            <span class="badge badge-info">PHP</span> 
+            <span class="badge badge-info">Laravel</span> 
+            <span class="badge badge-info">JAVA</span> 
+            <span class="badge badge-info">Javascript</span>
+            <span class="badge badge-info">MySQL</span> 
+            <span class="badge badge-info">Bootstrap</span> 
+            <span class="badge badge-info">GIT</span>
+            <span class="badge badge-info">JQuery</span>
+            <span class="badge badge-info">Ajax</span>
+          </p>
+          <p>
+            <span class="badge badge-dark">Analítica y organizada</span>
+            <span class="badge badge-dark">Autodidacta y de rápido aprendizaje</span>
+            <span class="badge badge-dark">Administración de proyectos</span>
+          </p>
+          <br>
+          <p><h4>Idiomas: </h4>
+            <span class="rombo"> ♦  </span><span class="Idioma">Español - Nativo </span> <br>
+            <span class="rombo"> ♦  </span><span class="Idioma">Inglés - Básico </span> <br>
+            <span class="rombo"> ♦  </span><span class="Idioma">Francés - Intermedio </span><br>
+          </p>
+
+          <hr>
+        </div>
+        <div class="col-4 viaje d-none d-sm-none d-lg-block" >
+          <br>
+          <br>
+          <br>
+          <img src="fondos/foto.jpg" alt="">
+          <small> <p> Viaje de intercambio a Quebec Canada</p>
+          <p> Estudie 4 meses en el Cegep de Rivier du Loup Techniques de l'informatique </p></small>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="container mt-5 pl-5 pr-5" id="proyectos">
+              <div class="col-12" style="color: #f0c9c9"><center> • • • • </center></div>
+
+    <br>
+    <br>
+      <h2>Proyectos</h2>
+    </div>
+
+
+    <div class="container mt-3 pl-5 pr-5">  
+      <div class="row">
+
+          <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="card h-100">
+            <img class="card-img-top" src="proyectos/socialhero.png" alt=""  >
+              <div class="card-body">
+
+                <h4 class="card-title">
+                  Donaciones
+                </h4>
+                <p class="card-text">
+                  <span class="badge badge-info">HTML5</span>
+                  <span class="badge badge-info">PHP</span> 
+                  <span class="badge badge-info">MySQL</span>
+                  <span class="badge badge-info">Ajax</span>
+                  <span class="badge badge-info">Bootstrap</span> 
+            <center> <a href="https://github.com/Darynka-Tapia/proyectohero" target="_blank"><i class="fab fa-github fa-3x" ></i></a></center>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="card h-100">
+              <img class="card-img-top" src="proyectos/gb_ba.png" alt="">
+              <div class="card-body">
+                <h4 class="card-title">
+                  Gestión Bibliotecaria
+                </h4>
+                <p class="card-text">
+                  <span class="badge badge-info">HTML5</span>
+                  <span class="badge badge-info">PHP</span> 
+                  <span class="badge badge-info">MySQL</span>
+                  <span class="badge badge-info">Javascript</span>
+                  <span class="badge badge-info">CSS3</span>
+                  <center> <a href="https://github.com/Darynka-Tapia/GB_BA" target="_blank"><i class="fab fa-github fa-3x" ></i></a></center>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="card h-100" >
+              <a href="#"><img class="card-img-top" src="proyectos/inventarios.png" alt=""></a>
+              <div class="card-body" style="position:relative;">
+                <h4 class="card-title">
+                  Administración de inventarios
+                </h4>
+                <p class="card-text" >
+                  <span class="badge badge-info">HTML5</span>
+            <span class="badge badge-info">PHP</span> 
+                  <span class="badge badge-info">MySQL</span>
+                  <span class="badge badge-info">Javascript</span>
+                  <span class="badge badge-info">CSS3</span>
+
+            <center> <a href="https://github.com/Darynka-Tapia/Inventario" target="_blank" ><i class="fab fa-github fa-3x" ></i></a></center>
+                </p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-12">
+
+          <center><a href="proyectos.php" class="btn btn-outline-success">Ver todos</a></center>
+        </div>
+      </div>
+      
+        
+
+    </div>
+
+    <div class="container mt-5 pl-5 pr-5" id="certificados">
+    <div class="col-12" style="color: #f0c9c9"><center> • • • • </center></div>
+
+    <br>
+    <br>
+      <h2>Certificados</h2>
+      <br>
+
+      <div class="row">
+        <div class="col-md-2 col-sm-0"></div>
+        <div class="col-md-8 col-sm-12">
+          <div id="carouselExampleCaptions" class="carousel slide mb-5" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="certificados/frances2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="background: #000; opacity: 0.7">
+                  <h5>Examen de certificación en francés</h5>
+                  <p>Examen realizado el año 2017 en la Universidad Tecnológica Emiliano Zapata UTEZ</p>
+                  <p><a href="https://drive.google.com/file/d/1BchuhOt1pFT3amRUXp8SY5TSqYT4CIx4/view?usp=sharing" target="_blank">→ Ver documento completo ←</a></p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="certificados/ingles2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="background: #000; opacity: 0.7">
+                  <h5>Beca proyecta 100 mil</h5>
+                  <p>Curso intensivo del idioma ingles en el nivel A2 en la universidad de New Orleans.</p>
+                  <p><a href="https://drive.google.com/file/d/15tWcHhKrIuqIuirakUvU_8qy3CeSOlP_/view?usp=sharing" target="_blank">→ Calificaciones ←</a></p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="certificados/AdministracionProyectos.png" class="d-block w-100" alt="..." style="width: 10%">
+                <div class="carousel-caption d-none d-md-block" style="background: #000; opacity: 0.7">
+                  <h5>Seminario de Aministración de proyectos</h5>
+                  <p> <a href="https://drive.google.com/open?id=1oKwsbQLmUWULd7v0KGRTFrbB2JDyXO3h" target="_blank">Introducción a Administración de proyectos</a>, 
+                    <a href="https://drive.google.com/open?id=1-vCxSWadGpsB4EbMTtFN2yrFyy9KWg6R" target="_blank">Costos y asignación de recursos</a>, 
+                    <a href="https://drive.google.com/open?id=1U7aMJXLkRYq9DIiLXypBzEvuHtt5tktK" target="_blank">Control y cierre del proyecto</a></p>
+                    <p> <a href="https://www.credential.net/31d468e4-dacc-4753-adae-cc3c59a2f5bf" target="_blank">→ Verificar credencial ←</a></p>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev" >
+                <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+              <!-- <button class="btn btn-primary">Ver más -></button> -->
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-0"></div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    </div>
+
+
+    <footer>
+      <div class="container-fluid footer">
+        <center>
+          <h4>Darynka Tapia</h4>
+          <a href="https://www.facebook.com/darynka.tapiaa" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+          <a href="https://www.twitter.com/darynka_tapia" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="https://www.linkedin.com/in/abril-tapia" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+          <a href="https://github.com/Darynka-Tapia" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+        </center>
+      </div>
+          
+    </footer>
+
+
+
+
+
+
+<script>  
+  
+</script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
